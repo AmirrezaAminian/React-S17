e019546399cd1edadd97bbeba806a90f1394336c
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-05-31 13:34:26 +0330

    handling the error

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,13 +6,20 @@ import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
 
-  const [meals , setMeals] = useState([])
+  const [meals , setMeals] = useState([]);
+  const [isLoading , setIsLoading] = useState(true);
+  const [httpError , setHttpError] = useState()
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://react-http-16bdb-default-rtdb.firebaseio.com/meals.json"
       );
+
+      if(!response.ok){
+        throw new Error('Something went wrong!')
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [] ;
@@ -27,10 +34,32 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadedMeals)
+      setIsLoading(false)
     };
 
-    fetchMeals();
+     fetchMeals().catch(err => {
+      setIsLoading(false);
+      setHttpError(err.message)
+     })
+
   }, []);
+
+  if(isLoading){
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if(httpError){
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
 
   const mealsList = meals.map((meal) => (
     <MealItem
